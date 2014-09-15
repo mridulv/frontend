52c646043bb67437634249096255b6025fb883e4
aitch_nu <harshinlko@gmail.com>
2014-09-15 15:17:43 +0530

    Created compare UI

--- js/index_new.js
+++ js/index_new.js
@@ -12,6 +12,16 @@ $(function () {
     $('#date_picker_start').datepicker({format: 'yyyy-mm-dd'});
     $('#date_picker_end').datepicker({format: 'yyyy-mm-dd'});
     $(".tm-input").tagsManager();
+    var config = {
+        '.chosen-select'           : {},
+        '.chosen-select-deselect'  : {allow_single_deselect:true},
+        '.chosen-select-no-single' : {disable_search_threshold:10},
+        '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+        '.chosen-select-width'     : {width:"95%"}
+    }
+    for (var selector in config) {
+        $(selector).chosen(config[selector]);
+    }
 });
 
 $(document.body).on('click', '.dropdown-menu li', function (event) {
